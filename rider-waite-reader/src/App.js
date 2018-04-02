99bd063ea8ff9bc2715ec362a1b9c77912349aa4
Jenner Hanni <jenner@wickerbox.net>
2018-04-02 12:49:59 -0700

    App displays images.

--- rider-waite-reader/src/App.js
+++ rider-waite-reader/src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The Rider-Waite Reader</h1>
+          <nav>
+            <ul>
+              <li>Get a Reading</li>
+              <li>History</li>
+              <li>Layouts</li>
+            </ul>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+				<section className="reading">
+					<h1>Get a Reading</h1>
+					<form>
+						<p>
+							Enter your question:
+							<input type="text" />
+						</p>
+						<p>
+							Which layout?<br />
+							<input type="radio" name="layout" value="one" checked /> One Card
+							<input type="radio" name="layout" value="three" /> Three Cards
+							<input type="radio" name="layout" value="directional" /> Directional
+							<input type="radio" name="layout" value="celticcross" /> Celtic Cross
+							<input type="radio" name="layout" value="treeoflife" /> Tree of Life
+						</p>
+						<p>
+							<input type="submit" value="Read the cards!" />
+						</p>
+					</form>
+				</section>
+
+				<section className="reading" style={{border: 0}}>
+					<h1>History</h1>
+					<p>Essay!</p>
+				</section>
+
+				<section>
+					<h1>Layouts</h1>
+				</section>
+
+				<section className="reading">
+					<h1>One Card</h1>
+					<p>Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo Lorem ipsum deo</p>
+					<div className="flex-row cards">
+						<div className="flex-col">
+							<div className="item"  style={{backgroundImage: `url("img/pents09.jpg")`}}>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
       </div>
     );
   }
