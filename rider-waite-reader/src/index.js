33c077e78ef2475471dac960f25eb51f135ed31b
Jenner Hanni <jenner@wickerbox.net>
2018-04-04 06:35:49 -0700

    Import Material-UI

--- rider-waite-reader/src/index.js
+++ rider-waite-reader/src/index.js
@@ -4,9 +4,14 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>,
+    <BrowserRouter>
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
+    </BrowserRouter>,
     document.getElementById('root')
 );
 
